deb771993e302704e1a092f5df80caa8412e15c0
dangerousjatt <73658726+dangerousjatt@users.noreply.github.com>
2021-08-08 09:36:30 +0530

    Update SpamBots.py

--- SpamBots.py
+++ SpamBots.py
@@ -562,29 +562,29 @@ async def spam(e):
     usage = "𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗥𝗮𝗶𝗱\n\nCommand:\n\n.raid <count> <Username of User>\n\n.raid <count> <reply to a User>\n\nCount must be a integer."
     if e.sender_id in SMEX_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
-            return await e.reply(usage, parse_mode=None, link_preview=None)
-        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        await e.get_reply_message()
-        if len(Ustad) == 2:
-            message = str(Ustad[1])
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        Sandhu = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(Sandhu) == 2:
+            message = str(Sandhu[1])
             print(message)
             a = await e.client.get_entity(message)
             g = a.id
             c = a.first_name
             username = f"[{c}](tg://user?id={g})"
-            counter = int(Ustad[0])
+            counter = int(Sandhu[0])
             for _ in range(counter):
                 reply = random.choice(RAID)
                 caption = f"{username} {reply}"
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, caption)
                     await asyncio.sleep(0.3)
-        elif e.reply_to_msg_id:
+        elif e.reply_to_msg_id:             
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
             g = b.id
             c = b.first_name
-            counter = int(Ustad[0])
+            counter = int(Sandhu[0])
             username = f"[{c}](tg://user?id={g})"
             for _ in range(counter):
                 reply = random.choice(RAID)
@@ -593,7 +593,10 @@ async def spam(e):
                     await e.client.send_message(e.chat_id, caption)
                     await asyncio.sleep(0.3)
         else:
-            await e.reply(usage, parse_mode=None, link_preview=None)
+            await e.reply(usage, parse_mode=None, link_preview=None )
+
+
+
 
 
 @idk.on(events.NewMessage(incoming=True))
