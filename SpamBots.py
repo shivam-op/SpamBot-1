d8c626b36bf704746550b8e29cdde1d5fcef4732
Aʟᴀɪɴ <77918121+infotechbro@users.noreply.github.com>
2021-08-13 10:04:32 +0530

    Update SpamBots.py

--- SpamBots.py
+++ SpamBots.py
@@ -801,7 +801,30 @@ async def restart(e):
 @ddk.on(events.NewMessage(incoming=True, pattern=r"\.help"))
 async def help(e):
     if e.sender_id in SMEX_USERS:
-        text = "𝗔𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀\n\n𝙐𝙩𝙞𝙡𝙨 𝘾𝙤𝙢𝙢𝙖𝙣𝙙:\n.ping\n.restart\n\n𝙐𝙨𝙚𝙧𝙗𝙤𝙩 𝘾𝙤𝙢𝙢𝙖𝙣𝙙:\n.bio\n.join\n.pjoin\n.leave\n\n𝙎𝙥𝙖𝙢 𝘾𝙤𝙢𝙢𝙖𝙣𝙙:\n.spam\n.delayspam\n.bigspam\n.raid\n.replyraid\n.dreplyraid\n\n\nFor more help regarding usage of plugins type plugins name"
+        text = """
+**Aᴠᴀɪʟᴀʙʟᴇ Cᴏᴍᴍᴀɴᴅs
+
+UsᴇʀBᴏᴛ Cᴏᴍᴍᴀɴᴅs:**
+
+`.bio` 
+`.join `
+`.pjoin `
+`.leave
+`
+**Uᴛɪʟs Cᴏᴍᴍᴀɴᴅ:**
+
+`.ping`
+`.restart`
+
+**Sᴘᴀᴍ Cᴏᴍᴍᴀɴᴅ:**
+`.spam`
+`.delayspam`
+`.bigspam`
+`.raid`
+`.relplyraid`
+`.dreplyraid`
+
+"""
         await e.client.send_message(e.chat.id,text, parse_mode=None, link_preview=None)
 
 
